cdfd529e22d7304eb24e1efff896d85275821fff
OdeiOtxoaErrrarte <ode_otx_flo@goierrieskola.org>
2025-12-12 10:41:13 +0100

    bbbbbbbbbbb

--- src/packmain/MainSistema.java
+++ src/packmain/MainSistema.java
@@ -11,7 +11,7 @@ public class MainSistema {
         if (rol != null) {
             SistemaFlujo.iniciarFlujo(rol);
         } else {
-            System.out.println("aaaaaaaaaaaaaa");
+            System.out.println("bbbbbbbbbbbbb");
         }
     }
 }
